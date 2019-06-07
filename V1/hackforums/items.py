c41d8d88ab0e634ed412d3c3ce2374b0ff496b81
Ryan Littlefield <ryan.j.littlefield@gmail.com>
2019-06-08 00:45:32 +0100

    Update items.py

--- V1/hackforums/items.py
+++ V1/hackforums/items.py
@@ -3,8 +3,6 @@
 import scrapy
 
 class HackforumsItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
     Title = scrapy.Field()
     Time = scrapy.Field()
     Lastpost = scrapy.Field()
